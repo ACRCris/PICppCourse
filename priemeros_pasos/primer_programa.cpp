8a0247d62f18f617b8e44d4e4bfb6c83286a47e2
ACRCopilot <crperezp@unal.edu.co>
2024-01-08 15:57:55 -0500

    Explicacion basica de funcion y fun main, tambien de la linea 21 6:51-14:20

--- priemeros_pasos/primer_programa.cpp
+++ priemeros_pasos/primer_programa.cpp
@@ -5,7 +5,7 @@
 
 //A las lineas de codigo que lee el compilador se les llama declaraciones o sentencias
 
-//#include <iostream> //Directiva de preprocesador include que permite incluir librerias
+#include <iostream> //Directiva de preprocesador include que permite incluir librerias
                     //iostream es una libreria estandar de c++ que permite operaciones de entrada y salida
 
 using namespace std;//las palabras reservada using namespace permite usar espacion de nombres.
@@ -14,11 +14,13 @@ using namespace std;//las palabras reservada using namespace permite usar espaci
                     //Es declaracion permite usar las funcionalidades de la libreria iostream sin tener que anteponer std::
                     
 
-int main(){
+int main(){ //funcion principal del cualquier programa en c++.
+            //Esta funcion como cualquier otra, empieza con una llave de apertura ( { ) y termina con una llave de cierre ( } )
+            //Funciones anidan bloques de codigo, que se ejecutan en orden secuencial de arriba abajo. Y tienen una entrada de parametros ()
 
-    cout<<"Hola alumnos que tal"<<endl; //Al comentar el include de la libreria iostream el compilador no reconoce la funcion cout de la cual pertenece a la libreria iostream
-                                        //cout es un funcionalidad de la libreria iostream que permite imprimir en pantalla
-
+    cout<<"Hola alumnos que tal"<<endl; //cout (character out) es un funcionalidad de la libreria iostream que permite imprimir en pantalla
+                                        //<< es un operador de insercion que permite insertar datos en el flujo de salida, es esta caso un string 
+                                        //endl agrega un salto de linea al flujo de salida
     return 0;
 
 }
