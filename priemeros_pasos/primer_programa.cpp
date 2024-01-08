cc1397b908fc63355b1ce7c2715ce6ddbdb3a598
ACRCopilot <crperezp@unal.edu.co>
2024-01-08 16:09:47 -0500

    Demostracion uso endl 20:50-22:20

--- priemeros_pasos/primer_programa.cpp
+++ priemeros_pasos/primer_programa.cpp
@@ -18,8 +18,8 @@ int main(){ //funcion principal del cualquier programa en c++.
             //Esta funcion como cualquier otra, empieza con una llave de apertura ( { ) y termina con una llave de cierre ( } )
             //Funciones anidan bloques de codigo, que se ejecutan en orden secuencial de arriba abajo. Y tienen una entrada de parametros ()
 
-    cout<<"Hola alumnos que tal"<<endl; //cout (character out) es un funcionalidad de la libreria iostream que permite imprimir en pantalla
-                                        //<< es un operador de insercion que permite insertar datos en el flujo de salida, es esta caso un string 
+    cout<<"Hola alumnos que tal"; //Se elimina el salto de linea, por lo que el texto se imprime en la misma linea
+    cout<<"me voy a programar en c++"<<endl;  
     
     //return 0; Creo que puede generar un error de compilacion en algunos compiladores
     //todas la funciones deben retornar un valor,exeptuando las void, en este caso la funcion main retorna un entero (int)
