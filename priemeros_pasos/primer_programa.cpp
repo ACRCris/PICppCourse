f31e2c8c524e1c4cd44d356c20a402c584460214
ACRCopilot <crperezp@unal.edu.co>
2024-01-08 15:38:14 -0500

    Inicio Video 5 Diseccion del primer programa 15/09/22

--- priemeros_pasos/primer_programa.cpp
+++ priemeros_pasos/primer_programa.cpp
@@ -1,9 +1,13 @@
+/*
+    Curos de c++ del canal pildoras informaticas.
+    Video 5 Diseccion primer programa 22/09/22
+*/
 #include <iostream> 
 using namespace std;
 
 int main(){
 
-    cout<<"Hola alumnos que tal"<<endl;
+    cout<<"Hola alumnos que tal"<<endl; 
 
     return 0;
 
