69644c557645e310c026df50ac78ba7675f788e0
ACRCopilot <crperezp@unal.edu.co>
2024-01-08 15:45:39 -0500

    se descomenta using namespace std y se comenta #include 6:06:35-6:51

--- priemeros_pasos/primer_programa.cpp
+++ priemeros_pasos/primer_programa.cpp
@@ -5,10 +5,10 @@
 
 //A las lineas de codigo que lee el compilador se les llama declaraciones o sentencias
 
-#include <iostream> //Directiva de preprocesador include que permite incluir librerias
+//#include <iostream> //Directiva de preprocesador include que permite incluir librerias
                     //iostream es una libreria estandar de c++ que permite operaciones de entrada y salida
 
-//using namespace std;//las palabras reservada using namespace permite usar espacion de nombres.
+using namespace std;//las palabras reservada using namespace permite usar espacion de nombres.
                     //los espacios de nombres son usado para organizar el codigo en bloques logicos y evitar conflictos de nombres
                     //std es un espacio de nombres que contiene las funcionalidades de la libreria iostream
                     //Es declaracion permite usar las funcionalidades de la libreria iostream sin tener que anteponer std::
@@ -16,7 +16,7 @@
 
 int main(){
 
-    cout<<"Hola alumnos que tal"<<endl; //Al comentar using namespace std; se genera un error pues se debe anteponer std::cout, pues ya no esta usando el espacio de nombres std en el codigo
+    cout<<"Hola alumnos que tal"<<endl; //Al comentar el include de la libreria iostream el compilador no reconoce la funcion cout de la cual pertenece a la libreria iostream
                                         //cout es un funcionalidad de la libreria iostream que permite imprimir en pantalla
 
     return 0;
