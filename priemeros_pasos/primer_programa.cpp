ca556744038fece37c3454a6c2ee89b97737a0b3
ACRCopilot <crperezp@unal.edu.co>
2024-01-08 15:39:48 -0500

    Explicacion include, using namespce std, cout 00:6:00

--- priemeros_pasos/primer_programa.cpp
+++ priemeros_pasos/primer_programa.cpp
@@ -2,12 +2,21 @@
     Curos de c++ del canal pildoras informaticas.
     Video 5 Diseccion primer programa 22/09/22
 */
-#include <iostream> 
-using namespace std;
+
+//A las lineas de codigo que lee el compilador se les llama declaraciones o sentencias
+
+#include <iostream> //Directiva de preprocesador include que permite incluir librerias
+                    //iostream es una libreria estandar de c++ que permite operaciones de entrada y salida
+
+using namespace std;//las palabras reservada using namespace permite usar espacion de nombres.
+                    //los espacios de nombres son usado para organizar el codigo en bloques logicos y evitar conflictos de nombres
+                    //std es un espacio de nombres que contiene las funcionalidades de la libreria iostream
+                    //Es declaracion permite usar las funcionalidades de la libreria iostream sin tener que anteponer std::
+                    
 
 int main(){
 
-    cout<<"Hola alumnos que tal"<<endl; 
+    cout<<"Hola alumnos que tal"<<endl; //cout es un funcionalidad de la libreria iostream que permite imprimir en pantalla
 
     return 0;
 
