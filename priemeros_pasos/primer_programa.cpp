3fe8304c764c6f032a468d45efc18a3f387ebf8e
ACRCopilot <crperezp@unal.edu.co>
2024-01-08 16:10:31 -0500

    fin video 5 22/09/22

--- priemeros_pasos/primer_programa.cpp
+++ priemeros_pasos/primer_programa.cpp
@@ -18,9 +18,9 @@ int main(){ //funcion principal del cualquier programa en c++.
             //Esta funcion como cualquier otra, empieza con una llave de apertura ( { ) y termina con una llave de cierre ( } )
             //Funciones anidan bloques de codigo, que se ejecutan en orden secuencial de arriba abajo. Y tienen una entrada de parametros ()
 
-    cout<<"Hola alumnos que tal"; //Se elimina el salto de linea, por lo que el texto se imprime en la misma linea
+    cout<<"Hola alumnos que tal"<<endl; //Se elimina el salto de linea, por lo que el texto se imprime en la misma linea
     cout<<"me voy a programar en c++"<<endl;  
-    
-    //return 0; Creo que puede generar un error de compilacion en algunos compiladores
+
+    return 0; //Creo que puede generar un error de compilacion en algunos compiladores
     //todas la funciones deben retornar un valor,exeptuando las void, en este caso la funcion main retorna un entero (int)
 }
