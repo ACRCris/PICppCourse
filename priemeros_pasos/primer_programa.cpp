c69391a02dd4238bf6f496d38375f1848df670c5
ACRCopilot <crperezp@unal.edu.co>
2024-01-08 16:06:44 -0500

    Explicacion de como funciona la devolucion de parametros 16:50-20:50

--- priemeros_pasos/primer_programa.cpp
+++ priemeros_pasos/primer_programa.cpp
@@ -7,6 +7,20 @@
 
 #include <iostream> //Directiva de preprocesador include que permite incluir librerias
                     //iostream es una libreria estandar de c++ que permite operaciones de entrada y salida
+
+using namespace std;//las palabras reservada using namespace permite usar espacion de nombres.
+                    //los espacios de nombres son usado para organizar el codigo en bloques logicos y evitar conflictos de nombres
+                    //std es un espacio de nombres que contiene las funcionalidades de la libreria iostream
+                    //Es declaracion permite usar las funcionalidades de la libreria iostream sin tener que anteponer std::
                     
 
-int main(){ std::cout<<"Hola alumnos que tal"<<std::endl;} //Es necesario usar el espacio de nombre std de forma explicita al quitar el using namespace std;
+int main(){ //funcion principal del cualquier programa en c++.
+            //Esta funcion como cualquier otra, empieza con una llave de apertura ( { ) y termina con una llave de cierre ( } )
+            //Funciones anidan bloques de codigo, que se ejecutan en orden secuencial de arriba abajo. Y tienen una entrada de parametros ()
+
+    cout<<"Hola alumnos que tal"<<endl; //cout (character out) es un funcionalidad de la libreria iostream que permite imprimir en pantalla
+                                        //<< es un operador de insercion que permite insertar datos en el flujo de salida, es esta caso un string 
+    
+    //return 0; Creo que puede generar un error de compilacion en algunos compiladores
+    //todas la funciones deben retornar un valor,exeptuando las void, en este caso la funcion main retorna un entero (int)
+}
