f8ff0e8f115a1223b84d73fbc9cc7010bc843630
ACRCopilot <crperezp@unal.edu.co>
2024-01-08 20:03:27 -0500

    incio video 8 15/10/22

---
+++ priemeros_pasos/trabajo_con_variables.cpp
@@ -0,0 +1,25 @@
+/*
+    Curos de c++ del canal pildoras informaticas.
+    Video 8 Ejemplo sencillo 15/10/22
+*/
+
+//A las lineas de codigo que lee el compilador se les llama declaraciones o sentencias
+
+#include <iostream> //Directiva de preprocesador include que permite incluir librerias
+                    //iostream es una libreria estandar de c++ que permite operaciones de entrada y salida
+
+using namespace std;//las palabras reservada using namespace permite usar espacion de nombres.
+                    //los espacios de nombres son usado para organizar el codigo en bloques logicos y evitar conflictos de nombres
+                    //std es un espacio de nombres que contiene las funcionalidades de la libreria iostream
+                    //Es declaracion permite usar las funcionalidades de la libreria iostream sin tener que anteponer std::
+                    
+
+int main(){ //funcion principal del cualquier programa en c++.
+            //Esta funcion como cualquier otra, empieza con una llave de apertura ( { ) y termina con una llave de cierre ( } )
+            //Funciones anidan bloques de codigo, que se ejecutan en orden secuencial de arriba abajo. Y tienen una entrada de parametros ()
+    
+   
+
+
+    //todas la funciones deben retornar un valor,exeptuando las void, en este caso la funcion main retorna un entero (int).
+}
