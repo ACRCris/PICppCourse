e2b3c31d047c61d1f36420828dd5cdd19dfb5b06
ACRCopilot <crperezp@unal.edu.co>
2024-01-08 19:45:14 -0500

    incio video 6 03/10/22

--- priemeros_pasos/primer_programa.cpp
+++ priemeros_pasos/primer_programa.cpp
@@ -1,6 +1,6 @@
 /*
     Curos de c++ del canal pildoras informaticas.
-    Video 5 Diseccion primer programa 22/09/22
+    Video 6 Tipos y variables 03/10/22
 */
 
 //A las lineas de codigo que lee el compilador se les llama declaraciones o sentencias
@@ -17,10 +17,9 @@ using namespace std;//las palabras reservada using namespace permite usar espaci
 int main(){ //funcion principal del cualquier programa en c++.
             //Esta funcion como cualquier otra, empieza con una llave de apertura ( { ) y termina con una llave de cierre ( } )
             //Funciones anidan bloques de codigo, que se ejecutan en orden secuencial de arriba abajo. Y tienen una entrada de parametros ()
+    
+    int salario = 3500; //declaracion de variable, es decir se reserva espacion en la RAM del ordenador, y inicializacion en  una misma linea
 
-    cout<<"Hola alumnos que tal"<<endl; //Se elimina el salto de linea, por lo que el texto se imprime en la misma linea
-    cout<<"me voy a programar en c++"<<endl;  
-
-    return 0; //Creo que puede generar un error de compilacion en algunos compiladores
+    return 0;
     //todas la funciones deben retornar un valor,exeptuando las void, en este caso la funcion main retorna un entero (int)
 }
