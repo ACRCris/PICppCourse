f3180716fc53db29162a968f3d28361564021bcf
ACRCopilot <crperezp@unal.edu.co>
2024-01-10 15:35:35 -0500

    inicio video 10 31/10/22

---
+++ ejercicio_recopilatorio_1.cpp
@@ -0,0 +1,10 @@
+/*
+    Curos de c++ del canal pildoras informaticas.
+    Video 10 Ejemplo sencillo 31/10/22
+
+    Simulador de compra de piso laminado.
+    Recibe los metros cuadrados de calidad media, luego recibe los metros cuadrados de calidad alta.
+    Calcula el subtotal de cada tipo de piso, luego calcula el total de cada piso y por ultimo calcula el total de la compra.
+    Muestra el valor en consola.
+*/
+
