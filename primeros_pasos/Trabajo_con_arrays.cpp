23afbacd5f86313c44ced2bb4b9832201d701a6d
ACRCopilot <crperezp@unal.edu.co>
2024-01-10 17:23:48 -0500

    print en consola la posicion 7 del array 6:13-7:40.

--- primeros_pasos/Trabajo_con_arrays.cpp
+++ primeros_pasos/Trabajo_con_arrays.cpp
@@ -8,12 +8,14 @@ using namespace std;// Uso de espacio de nombre std implicito en todo el codigo
 
 int main(){
 
+
+
     int edades[10] = {15,20,25};//Declaracion e inicializacion de array de tipo entero especificando el tamaño del array pero sin llenar todas la posciones
                                 //la indexacion de los elementos del array empieza en 0. 
                                 //las posciones 0,1,2 tienen las respectivas edades 15,20,25 y las posiciones 3,4,5,6,7,8,9 tienen el valor 0
                                 //Es distinto el tamanio del array y el ultimo indice del array, pues la indexacion empieza en 0
-                                
-    cout<<edades[2]<<endl; //imprime en consola el valor 20
+
+    cout<<edades[7]<<endl; //imprime en consola el valor 0
 
     return 0;   
 
