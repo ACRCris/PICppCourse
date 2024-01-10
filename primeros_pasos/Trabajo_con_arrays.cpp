54a65cfe2e59923aff2999ee34271627f4975c56
ACRCopilot <crperezp@unal.edu.co>
2024-01-10 17:12:21 -0500

    Incio video 12 declaracion e inicializacion de arrays 30/11/22

---
+++ primeros_pasos/Trabajo_con_arrays.cpp
@@ -0,0 +1,22 @@
+/*
+    Curos de c++ del canal pildoras informaticas.
+    Video 12 Declaracion e inicializacion de arrays 30/11/22
+*/
+
+#include <iostream> //Directiva de preprocesador include para incluir iostream libreria estandar de c++ que permite operaciones de entrada y salida
+using namespace std;// Uso de espacio de nombre std implicito en todo el codigo
+
+int main(){
+
+   
+
+    return 0;
+
+    /*
+        C++ No es WORA (Write Once Run Anywhere) como Java, es decir que la ejecuion de un programa en c++ depende de la arquitectura del computador
+    */ 
+
+}
+
+
+
