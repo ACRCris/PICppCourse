73e2182a04bd05a0cc1a8b49f8b9a1a561088908
ACRCopilot <crperezp@unal.edu.co>
2024-01-11 11:20:02 -0500

    division no entera de solo numero int da un tipo int aunque se asigne a un double  11:25-13:05

--- primeros_pasos/Trabajo_con_operadores.cpp
+++ primeros_pasos/Trabajo_con_operadores.cpp
@@ -12,12 +12,13 @@ int main(){
 
     int a=3;
     
-    int b=20;
+    int b=12;
 
     int c=30;
 
-    double media=(a+b+c)/3; //La division es entera, por lo que el resultado sera 17 un entero.
-
+    double media=(a+b+c)/3; //Ejemplo de la division no entera, de la suma de 3 numeros enteros, el resultado es un numero entero, por lo que se pierde la parte decimal.
+                            //Esto debido a la conversion implicita de tipos de datos, pues c++ convierte al tipo mas preciso o amplio pero como todos son tipo int el resultado es int.
+                            
     cout<<"La media de los 3 valores es: "<<media<<endl;
     /*
         C++ No es WORA (Write Once Run Anywhere) como Java, es decir que la ejecuion de un programa en c++ depende de la arquitectura del computador
