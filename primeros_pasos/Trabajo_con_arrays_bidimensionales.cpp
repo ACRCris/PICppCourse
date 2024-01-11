3c80544175487cda28020111d6e70986165749c6
ACRCopilot <crperezp@unal.edu.co>
2024-01-10 19:50:50 -0500

    se reasigna un nuevo elemento el poscion 0,2 del array bidimensional 10:00-11:30

--- primeros_pasos/Trabajo_con_arrays_bidimensionales.cpp
+++ primeros_pasos/Trabajo_con_arrays_bidimensionales.cpp
@@ -13,6 +13,8 @@ int main(){
     //Los numero a la derecha van llenando en orden de izq-dehc arriba-abajo.
     int mi_matriz [4][5] = {15,25,8,-7,92,77,12,11,44,56,59,43,78,12,43,95,12,87,33};
 
+    mi_matriz[0][2] = 125; //asignacion de un nuevo valor a un elemento del array
+
     cout<< mi_matriz[0][2]<<endl;  //imprime en consola el valor 8, que esta en la fila 0 y columna 2.
     
     return 0;   
