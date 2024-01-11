c61b6fa600dff0349241dc4593f16574509d0f31
ACRCopilot <crperezp@unal.edu.co>
2024-01-10 20:40:27 -0500

    ejemplo vectores de enteros y caracteres 00:00-05:54

--- primeros_pasos/Trabajo_con_vectore.cpp
+++ primeros_pasos/Trabajo_con_vectore.cpp
@@ -4,12 +4,18 @@
 */
 
 #include <iostream> //Directiva de preprocesador include para incluir iostream libreria estandar de c++ que permite operaciones de entrada y salida
+#include <vector> //Directiva de preprocesador include para incluir vector libreria estandar de c++ que permite operaciones con vectores
+
 using namespace std;// Uso de espacio de nombre std implicito en todo el codigo
 
 int main(){
 
-    
-
+    vector <int> records;   //Declaracion de un vector de tipo int, si generea es por que se necesita incluir la libreria vector.
+                            //Se declaran con la palabra reservada vector seguido de <tipo de dato> y el nombre del vector.
+                            //Los vectores son dinamicos, es decir que se pueden modificar su tamaño en tiempo de ejecucion.
+                            //Cuando no se especifica el tamaño del vector esto son vacios, es decir que no tienen elementos.
+                            
+    vector <char> letras;   //Declaracion de un vector de tipo char.
 
     return 0;
 
