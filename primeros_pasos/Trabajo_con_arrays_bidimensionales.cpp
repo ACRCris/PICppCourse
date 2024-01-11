60f3bab29e9d1506647598819181c6c7b2e70f4d
ACRCopilot <crperezp@unal.edu.co>
2024-01-10 20:00:10 -0500

    Se llenan y se muestra los datos de array usando bucles for 11:30-18:30

--- primeros_pasos/Trabajo_con_arrays_bidimensionales.cpp
+++ primeros_pasos/Trabajo_con_arrays_bidimensionales.cpp
@@ -11,13 +11,31 @@ int main(){
     //Declaracion e inicializacion de array bidimensional.
     //El primer numero entre [] indica el numero de filas y el segundo numero entre [] indica el numero de columnas. Este array tiene 4 filas 5 columnas.
     //Los numero a la derecha van llenando en orden de izq-dehc arriba-abajo.
-    int mi_matriz [4][5] = {15,25,8,-7,92,77,12,11,44,56,59,43,78,12,43,95,12,87,33};
+    int mi_matriz [4][5];//Se usara bucles anidados para rellenar las posiciones del array.
 
-    mi_matriz[0][2] = 125; //asignacion de un nuevo valor a un elemento del array
 
-    cout<< mi_matriz[0][2]<<endl;  //imprime en consola el valor 8, que esta en la fila 0 y columna 2.
-    
-    return 0;   
+    for(int fila = 0; fila < 4; fila++){//se inicializa la variable fila en 0, se evalua la condicion fila < 4, si es verdadera se ejecuta el bloque de codigo y se incrementa fila en 1.
+
+        for(int columna = 0; columna < 5; columna++){//se inicializa la variable columna en 0, se evalua la condicion columna < 5, si es verdadera se ejecuta el bloque de codigo y se incrementa columna en 1.
+
+            cin>>mi_matriz[fila][columna];
+
+        }//por cada entrada en el primer bucle for, este bucle for se ejecuta 5 veces.
+    }
+
+    cout<< "Visualizamos ahora los valores almacenados en el array"<<endl;
+
+    for(int fila = 0; fila < 4; fila++){//se inicializa la variable fila en 0, se evalua la condicion fila < 4, si es verdadera se ejecuta el bloque de codigo y se incrementa fila en 1.
+
+        for(int columna = 0; columna < 5; columna++){//se inicializa la variable columna en 0, se evalua la condicion columna < 5, si es verdadera se ejecuta el bloque de codigo y se incrementa columna en 1.
+
+            cout<<mi_matriz[fila][columna]<<" "; 
+
+        }
+
+        cout<<endl;//cada cinco iteraciones del bucle for se imprime un salto de linea, para que se visualice como una matriz.
+    }
+
 
     /*
         C++ No es WORA (Write Once Run Anywhere) como Java, es decir que la ejecuion de un programa en c++ depende de la arquitectura del computador
