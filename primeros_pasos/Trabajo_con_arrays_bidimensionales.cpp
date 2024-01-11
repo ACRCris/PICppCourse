0d12f2ebc911b2e449dd183967a1dc13c995cd5e
ACRCopilot <crperezp@unal.edu.co>
2024-01-10 19:18:48 -0500

    ejemplo array de bidimensional 4X5 8:00-10:00

--- primeros_pasos/Trabajo_con_arrays_bidimensionales.cpp
+++ primeros_pasos/Trabajo_con_arrays_bidimensionales.cpp
@@ -8,8 +8,13 @@ using namespace std;// Uso de espacio de nombre std implicito en todo el codigo
 
 int main(){
 
-    
+    //Declaracion e inicializacion de array bidimensional.
+    //El primer numero entre [] indica el numero de filas y el segundo numero entre [] indica el numero de columnas. Este array tiene 4 filas 5 columnas.
+    //Los numero a la derecha van llenando en orden de izq-dehc arriba-abajo.
+    int mi_matriz [4][5] = {15,25,8,-7,92,77,12,11,44,56,59,43,78,12,43,95,12,87,33};
 
+    cout<< mi_matriz[0][2]<<endl;  //imprime en consola el valor 8, que esta en la fila 0 y columna 2.
+    
     return 0;   
 
     /*
