c65bd3ca8f107e139f32e8f372b6d71cac4fb25b
ACRCopilot <crperezp@unal.edu.co>
2024-01-11 11:25:27 -0500

    division no entera de las suma de enteros con un tipo double  13:05-15:55

--- primeros_pasos/Trabajo_con_operadores.cpp
+++ primeros_pasos/Trabajo_con_operadores.cpp
@@ -10,16 +10,17 @@ using namespace std;// Uso de espacio de nombre std implicito en todo el codigo
 
 int main(){ 
 
-    int a=3;
+    int a=10;
     
-    int b=12;
+    double b=12;
 
     int c=30;
 
-    double media=(a+b+c)/3; //Ejemplo de la division no entera, de la suma de 3 numeros enteros, el resultado es un numero entero, por lo que se pierde la parte decimal.
-                            //Esto debido a la conversion implicita de tipos de datos, pues c++ convierte al tipo mas preciso o amplio pero como todos son tipo int el resultado es int.
-                            
+    double media=(a+b+c)/3; //Ejemplo de la division no entera, de la suma de almenos un double y un int, el resultado es un double, pues es el tipo de dato mas espacioso y preciso.
+
     cout<<"La media de los 3 valores es: "<<media<<endl;
+
+    cout<<"El tipo de dato de media es: "<<typeid(media).name()<<endl;
     /*
         C++ No es WORA (Write Once Run Anywhere) como Java, es decir que la ejecuion de un programa en c++ depende de la arquitectura del computador
         C++ a diferencia de JAVA no maneja la excepcion ArrayIndexOutOfBoundsException, por lo que si se intenta acceder a una posicion de un array que sea declarado se obtendra los que se conoce como udnefined behavior.
