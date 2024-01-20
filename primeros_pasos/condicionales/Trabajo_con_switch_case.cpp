a0108fd42728646c99d13c9bf4add4afcff51f1e
ACRCopilot <crperezp@unal.edu.co>
2024-01-20 14:23:55 -0500

    Explicacion switch-case y ambito de variables 05:55-17:45

--- primeros_pasos/condicionales/Trabajo_con_switch_case.cpp
+++ primeros_pasos/condicionales/Trabajo_con_switch_case.cpp
@@ -9,5 +9,42 @@ using namespace std;
 
 int main(){
     
-       
+    int opcion;
+
+    cout<<"Menu de opciones"<<endl;
+
+    cout<<"1. Mostrar mensaje"<<endl;
+    cout<<"2. Calcular una suma"<<endl;
+    cout<<"3. Salir del programa"<<endl;
+    cout<<"Introduce la opcion deseada: (1, 2 o 3): "<<endl;
+
+    cin>>opcion;
+
+    switch(opcion){
+        case 1: 
+
+                cout<<"Hola alumnos de C++"<<endl;
+
+                break;
+
+        case 2: 
+
+                int a, b, suma;
+
+                cout<<"Introduce dos numeros: "<<endl;
+
+                cin>>a>>b; //Introduccion por consola de dos numeros enteros a y b, con el operador >> se puede introducir varios valores en una misma linea
+
+                cout<<"La suma de "<<a<<" y "<<b<<" es "<<a+b<<endl;    
+
+                break;
+        
+        case 3: 
+
+                cout<<"Saliendo del programa..."<<endl;
+
+        default: 
+                cout<<"Opcion no valida"<<endl;
+    }
+
 }
