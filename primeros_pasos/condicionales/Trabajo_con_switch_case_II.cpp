24df07e2fd1eb44bf69e0af540bfb0c236224929
ACRCopilot <crperezp@unal.edu.co>
2024-01-20 14:32:32 -0500

    inicio  23 Switch-Case II  08/05/23

---
+++ primeros_pasos/condicionales/Trabajo_con_switch_case_II.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+/*
+    Curos de c++ del canal pildoras informaticas.
+    Video 23 Switch-Case 08/05/23
+*/
+
+
+int main(){
+    
+    
+
+    return 0;
+
+}
