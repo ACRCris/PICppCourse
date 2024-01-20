63f9a2e6f69168e18a928364e027fdeeb7979064
ACRCopilot <crperezp@unal.edu.co>
2024-01-20 15:42:51 -0500

    Ejemplo de verificacion si un numero es par o no con condicional if 03:10-08:00

--- primeros_pasos/condicionales/Trabajo_con_operador_ternario.cpp
+++ primeros_pasos/condicionales/Trabajo_con_operador_ternario.cpp
@@ -8,8 +8,18 @@ using namespace std;
 
 
 int main(){
-   
 
-    return 0;
+        int num; 
+
+        cout<<"Introduce un n° entero: "<<endl;  
+
+        cin>>num;
+
+        if(num%2==0)
+                cout<<"El numero "<<num<<" es par"<<endl;
+        else
+                cout<<"El numero "<<num<<" es impar"<<endl;
+
+        return 0;
 
 }
