ea5af011c17b017eb01775cb654f51efa2bc27bf
ACRCopilot <crperezp@unal.edu.co>
2024-01-20 15:56:28 -0500

    fin video 24 Operador ternario 12/05/23

--- primeros_pasos/condicionales/Trabajo_con_operador_ternario.cpp
+++ primeros_pasos/condicionales/Trabajo_con_operador_ternario.cpp
@@ -15,7 +15,7 @@ int main(){
 
         cin>>num;
 
-        //La condicion creada en el anterio commit se puede simplificar con el operador ternario.
+        //La condicion creada en el anterio commit se puede simplificar con el operador ternario
 
         string resultado = (num%2==0) ? " es par" : " es impar";        //El operador ternario es una estructura de control que permite simplificar la estructura if-else, es decir, es una estructura de control que permite evaluar una condicion y ejecutar un codigo u otro en funcion del resultado de la evaluacion de la condicion.
                                                                         // A diferencia de if no es una sentencia si uno una expresion que permite almacenar el resultado en una variable.
