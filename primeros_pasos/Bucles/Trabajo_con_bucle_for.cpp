212d116a79e7878fc3efeef1a2296bc55d19b693
ACRCopilot <crperezp@unal.edu.co>
2024-01-20 16:20:32 -0500

    segundo ejemplo bucle for 15:00-18:15

--- primeros_pasos/Bucles/Trabajo_con_bucle_for.cpp
+++ primeros_pasos/Bucles/Trabajo_con_bucle_for.cpp
@@ -9,10 +9,11 @@ using namespace std;
 
 int main(){
 
-    for(int i=0; i<10; i++){    //La sintaxis del bucle for es la siguiente: for(inicializacion; condicion; incremento){codigo a ejecutar}
-                                
-        cout<<"Hola Alumnos"<<i<<endl;
-    
+    int edades[] = {25,35,80.50,27}; //Declaracion e inicializacion de un array de enteros
+
+    for (int i = 0; i < 5; i++) //La sintaxis del bucle for es la siguiente: for (inicializacion; condicion; incremento) {codigo a ejecutar}
+    {
+        cout<<"Edad "<<i<<" es: "<<edades[i]<<endl;
     }
 
     return 0;
