fd093c93838065a552c67726e1f3e5d90acf0e09
ACRCopilot <crperezp@unal.edu.co>
2024-01-20 16:24:59 -0500

    Como recorrer un array bidimensional usando bucles for anidados 18:15-23:41

--- primeros_pasos/Bucles/Trabajo_con_bucle_for.cpp
+++ primeros_pasos/Bucles/Trabajo_con_bucle_for.cpp
@@ -9,11 +9,16 @@ using namespace std;
 
 int main(){
 
-    int edades[] = {25,35,80.50,27}; //Declaracion e inicializacion de un array de enteros
-
+    int edades[3][5] = {{25,35,80.50,27}, //Declaracion e inicializacion de un array bidimensional de 3 filas y 5 columnas de tipo entero
+                        {17,19,81,52,83},
+                        {7,9,18,23,19}};
+   
     for (int i = 0; i < 5; i++) //La sintaxis del bucle for es la siguiente: for (inicializacion; condicion; incremento) {codigo a ejecutar}
     {
-        cout<<"Edad "<<i<<" es: "<<edades[i]<<endl;
+       for (int j = 0; j < 3; j++) //El bucle for se puede anidar, es decir, se puede poner un bucle for dentro de otro bucle for
+       {
+           cout<<"La edad de la fila "<<j<<" y la columna "<<i<<" es: "<<edades[j][i]<<endl;
+       } //Por cada iteracion del bucle for inicial este bucle for se ejecutara 3 veces
     }
 
     return 0;
