e83b33dc80d974b849af7fe01e8c777c387d2e54
ACRCopilot <crperezp@unal.edu.co>
2024-01-20 15:39:44 -0500

    inicio video 24 Operador ternario 12/05/23

---
+++ primeros_pasos/condicionales/Trabajo_con_operador_ternario.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+/*
+    Curos de c++ del canal pildoras informaticas.
+    Video 24 Operador Ternario 12/05/23
+*/
+
+
+int main(){
+   
+
+    return 0;
+
+}
