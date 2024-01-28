ad535103da08b2a995df1845cf5c99bb742cbf6d
ACRCopilot <crperezp@unal.edu.co>
2024-01-27 20:17:33 -0500

    fin video  27 Bucle foreach 15/06/23

---
+++ primeros_pasos/Bucles/Trabajo_con_bucle_while.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+    Curos de c++ del canal pildoras informaticas.
+    Video 28 Bucle While 26/06/23
+*/
+
+
+int main(){
+    
+    
+    return 0;
+}
