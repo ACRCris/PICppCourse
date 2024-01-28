62353ba7d05aa49b4898800d807761197510db08
ACRCopilot <crperezp@unal.edu.co>
2024-01-27 20:24:59 -0500

    explicacion y ejemplo bucle while 6:50-14:41

--- primeros_pasos/Bucles/Trabajo_con_bucle_while.cpp
+++ primeros_pasos/Bucles/Trabajo_con_bucle_while.cpp
@@ -10,6 +10,18 @@ using namespace std;
 
 int main(){
     
+    int i {1};
+
+    while (i <= 10) //El bucle while es bucle indeterminado, es decir, no se sabe cuantas veces se va a ejecutar el bucle
+    {
+        if(i % 2 == 0){ //Si el resto de la division de i entre 2 es 0, es decir, si i es par
+            
+            cout<<"El numero "<<i<<" es par"<<endl;
+        }
+        
+        i++; //Incrementa el valor de i en 1
+
+    }
     
     return 0;
 }
