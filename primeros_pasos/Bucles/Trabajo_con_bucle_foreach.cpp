defbb81798f9dff88229ec1051eafc32cc27a124
ACRCopilot <crperezp@unal.edu.co>
2024-01-27 19:59:08 -0500

    inicio video  27 Bucle for 15/06/23

--- primeros_pasos/Bucles/Trabajo_con_bucle_foreach.cpp
+++ primeros_pasos/Bucles/Trabajo_con_bucle_foreach.cpp
@@ -9,14 +9,6 @@ using namespace std;
 
 int main(){
 
-    int edades[] = {25,35,80.50,27}; //Declaracion e inicializacion de un array de tipo entero.
-
-    for(int edad:edades){   //La sintaxis del bucle for each es la siguiente: for (tipo_de_dato variable:array) {codigo a ejecutar}
-                            //perdemos la informacion de la posicion del array, solo podemos acceder al valor de cada elemento del array.
-        cout<<edad<<endl;   
-    }
-                        
-   
     
     return 0;
 }
